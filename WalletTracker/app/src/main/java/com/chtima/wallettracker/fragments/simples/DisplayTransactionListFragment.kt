d82c1febc89ee61b2dba741412f5386112843c78
Ch-Tima <chizhevskii.tima@gmail.com>
2025-04-03 20:34:40 +0200

    Beta TransactionFilter

--- WalletTracker/app/src/main/java/com/chtima/wallettracker/fragments/simples/DisplayTransactionListFragment.kt
+++ WalletTracker/app/src/main/java/com/chtima/wallettracker/fragments/simples/DisplayTransactionListFragment.kt
@@ -9,11 +9,17 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.chtima.wallettracker.R
 import com.chtima.wallettracker.adapters.TransactionAdapter
-import com.chtima.wallettracker.fragments.HomeFragment
+import com.chtima.wallettracker.models.Category
 import com.chtima.wallettracker.models.CategoryWithTransactions
 import com.chtima.wallettracker.models.Transaction
 import com.chtima.wallettracker.viewModels.CategoryViewModel
+import java.util.Date
 
+
+/**
+ * Fragment для вывода спика транзакций с возможностью влияния на выводимую информацию через @
+ *
+ * **/
 class DisplayTransactionListFragment : Fragment() {
 
     //ViewModels
@@ -21,7 +27,8 @@ class DisplayTransactionListFragment : Fragment() {
 
     //other
     private lateinit var adapter: TransactionAdapter
-    private val categoryWithTransactions: MutableList<CategoryWithTransactions> = ArrayList()
+    private var categoryWithTransactions: List<CategoryWithTransactions> = emptyList()
+    private lateinit var f: TransactionFilter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +41,7 @@ class DisplayTransactionListFragment : Fragment() {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_display_transaction_list, container, false)
 
+        // адаптер для визуализации
         adapter = TransactionAdapter(requireContext(), toTransactionList().toMutableList())
         val recyclerView: RecyclerView = v.findViewById(R.id.list_transaction)//need only one time
         recyclerView.adapter = adapter
@@ -43,11 +51,15 @@ class DisplayTransactionListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //содания VM и зпрос данных
         categoryVM = ViewModelProvider(this)[CategoryViewModel::class]
         categoryVM.getCategoriesWithTransactionsByUser().observe(viewLifecycleOwner){
-            categoryWithTransactions.clear()
-            categoryWithTransactions.addAll(it)
-            adapter.updateList(toTransactionList())
+            categoryWithTransactions = it.toList()
+            f = TransactionFilter(categoryWithTransactions, {
+                adapter.updateList(toTransactionList(it))
+            })
+            //f.byTitle("c").apply()//TEST
         }
     }
 
@@ -56,10 +68,61 @@ class DisplayTransactionListFragment : Fragment() {
         fun newInstance() = DisplayTransactionListFragment().apply {}
     }
 
+    public fun filter() = f
+
+    class TransactionFilter (private val categoryWithTransactions: List<CategoryWithTransactions>,
+               private val callback: (List<CategoryWithTransactions>) -> Unit){
+
+        private var filtered: List<CategoryWithTransactions> = categoryWithTransactions.toList()
+
+        fun byTitle(text: String): TransactionFilter {
+            filtered = filtered.map { cwt ->
+                cwt.copy(
+                    transactions = cwt.transactions.filter { it.title.contains(text, ignoreCase = true) }
+                )
+            }.filter { it.transactions.isNotEmpty() }
+            return this
+        }
+
+        fun byNote(note: String): TransactionFilter {
+            filtered = filtered.map { categoryWithTransactions ->
+                categoryWithTransactions.copy(
+                    transactions = categoryWithTransactions.transactions.filter { it.note?.contains(note, ignoreCase = true) == true }
+                )
+            }.filter { it.transactions.isNotEmpty() }
+            return this
+        }
+        fun bySum(min: Double, max: Double): TransactionFilter {
+            filtered = filtered.map { categoryWithTransactions ->
+                categoryWithTransactions.copy(
+                    transactions = categoryWithTransactions.transactions.filter { it.sum in min..max }
+                )
+            }.filter { it.transactions.isNotEmpty() }
+            return this
+        }
+
+
+        public fun apply(){
+            callback(filtered)
+        }
+
+    }
     /**
      * @return all Transaction from List&lt;CategoryWithTransactions&gt;
-     * @see HomeFragment#categoryWithTransactions */
+     * */
     private fun toTransactionList():List<Transaction>{
-        return categoryWithTransactions.flatMap { it.transactions }
+        return toTransactionList(this.categoryWithTransactions)
     }
+    private fun toTransactionList(l: List<CategoryWithTransactions>):List<Transaction>{
+        return l.flatMap { it.transactions }
+    }
+
+    class SortingCriteria{
+        var title: String = ""
+        var note: String = ""
+        var categories: List<Category> = ArrayList()
+        var date: Date? = null
+        var transactionType : Transaction.TransactionType? = null
+    }
+
 }
